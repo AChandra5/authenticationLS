6eff3557d3a2cdb6adf7ff23f9cc77d2a1d4047c
Akhil Chandra <87477447+AChandra5@users.noreply.github.com>
2023-01-06 17:52:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,65 +1,55 @@
- const submitBtn = document.getElementById("submit")
- let userDetails = {}
+const submitBtn = document.getElementById("submit")
+const errorElement = document.getElementById("error")
 
-
-
-//generating a random access token
-let token
- function generateAccessToken() {
-    // Generate a random number using Math.random()
-     token = Math.random().toString(36).substring(2);
-    return token;
-  }
-
-
-
- const signUp = () => {
-
-    userDetails.name = document.getElementById("fullname").value
-    userDetails.email = document.getElementById("email").value
-    userDetails.accessToken = generateAccessToken()
-    if (document.getElementById("password").value === document.getElementById("confirmPassword").value) {
-        userDetails.password = document.getElementById("password").value
-    }
-    console.log("userdetails ", userDetails)
-    
-    //conditional display of error messages.
-    if ((userDetails.name == "") || (userDetails.email == "") || (userDetails.password == "")) {
-        document.getElementById("error-feedback").style.display = 'block'
-        document.getElementById("sucess-feedback").style.display = 'none'
-    } else {
-        document.getElementById("error-feedback").style.display = 'none'
-        document.getElementById("sucess-feedback").style.display = 'block'
-        // window.location.href = './profilepage.html'
-
-    }
-
-            //storing the user details in localstorage
-            localStorage.setItem('user', JSON.stringify({
-                name: userDetails.name,
-                email: userDetails.email,
-                password: userDetails.password,
-                accessToken : userDetails.accessToken
-            }))
-
-            const user = JSON.parse(localStorage.getItem('user'))
-    // console.log("user", user)
-    // window.location.href = './profilepage.html'
-
-    // if (JSON.parse(localStorage.user).accessToken) {
-        // If the user is logged in, redirect to the profile page
-        // window.location.href = './profilepage.html'
-        // document.getElementById("name-profile").innerHTML = `Hii`
-    //   }
-   
+const getInputValueById = (id) => {
+    return document.getElementById(id).value;
 }
 
-submitBtn.onclick=signUp
-// console.log(JSON.parse(localStorage.user).name)
+
+const handleSubmit = () => {
+    errorElement.textContent = ''
+
+    const inputIds = ["fullname","email","password","confirmPassword"]
+    const user = {}
+    let isError = false;
+
+    inputIds.forEach((id) => {
+        const userData = getInputValueById(id)
+
+        if(!userData){
+            isError = true
+            return
+        }
+      
+        user[id] = userData
+    })
+    if(isError){
+        errorElement.textContent = 'Error: All fields are mandatory'
+        errorElement.style.color='red'
+        return 
+    }
+    //password mismatch error display
+    if(user.password !== user.confirmPassword){
+        errorElement.textContent = 'Error: Password and confirm password must match'
+        errorElement.style.color='red'
+        return 
+    }
+//generate access token and add to our user object
+    const access_token = (Math.random()+1).toString(36).substring(1)
+    user.access_token = access_token
+
+    console.log("userdetails", user);
+
+    //local storage of our user
+    localStorage.setItem('user',JSON.stringify(user))
+
+    //redirect user from signup to profile page
+    // window.location.href = "./profilepage.html"
+    window.open("./profilepage.html","_self")
+}
 
 
- 
-
-
-
-
+submitBtn.addEventListener("click",handleSubmit)
+document.addEventListener("DOMContentLoaded",()=>{
+    console.log("loaded");
+})
